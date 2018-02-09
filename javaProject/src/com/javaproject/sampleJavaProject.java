2b36c8956ccee731c6c7e92387029495cd918d7b
bhavani <bhavani.devjava@gmail.com>
2018-02-09 09:05:45 -0800

    first commit

---
+++ javaProject/src/com/javaproject/sampleJavaProject.java
@@ -0,0 +1,10 @@
+package com.javaproject;
+
+public class sampleJavaProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello java project");
+	}
+
+}
